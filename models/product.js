442b6ee2a26019d6ee8aec2e2a8c91be49b0ff61
yasmeeneb1999 <yyasmina.klye@gmail.com>
2026-02-26 16:48:02 +0300

    uploud folder

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String },
+    price: { type: Number }
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+module.exports = mongoose.model("Product", ProductSchema)
