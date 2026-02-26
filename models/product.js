820adf441dfad34e42db0378564a6269aac4ff0b
Fatii78 <fatii78jj@gmail.com>
2026-02-26 17:40:39 +0300

    delete file

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String },
-    price: { type: Number }
+    price: { type: Number },
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
